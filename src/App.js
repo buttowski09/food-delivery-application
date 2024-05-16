846d80791a4b96fea927a1cf009e92562dcf7cb5
raghul <raghul.0719@gmail.com>
2024-05-16 23:19:10 +0530

    mernappstarting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import Navbar from './components/Navbar';
+import Home from './screens/Home';
 
 function App() {
   return (
-    <div ><Navbar/></div>
+    <div ><Home></Home></div>
   );  
 }
 
